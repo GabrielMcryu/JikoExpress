aa863e50d545381f36c7c6fc28f375d2042e1574
GabrielMcryu <gabrielmcryu@gmail.com>
2023-08-07 22:59:13 +0300

    Able to connect to mongodb server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 // IMPORTS FROM PACKAGES
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+// mongodb credentials
+const username = process.env.APP_USERNAME;
+const password = process.env.APP_PASSWORD;
 
 // INIT
 const PORT = process.env.PORT || 3000;
 const app = express();
+const DB = `mongodb+srv://${username}:${password}@cluster0.8sycumm.mongodb.net/?retryWrites=true&w=majority`;
 
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('Connection Successful');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`connected at port ${PORT}`);
 });
