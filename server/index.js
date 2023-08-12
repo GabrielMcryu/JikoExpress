fdc868178cbe5a4645079f55d723942975c792cc
GabrielMcryu <gabrielmcryu@gmail.com>
2023-08-12 10:48:41 +0300

    Created auth endpoints

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// IMPORTS FROM OTHER FILES
+const authRouter = require('./routes/auth');
+
 // mongodb credentials
 const username = process.env.APP_USERNAME;
 const password = process.env.APP_PASSWORD;
@@ -11,6 +14,10 @@ const password = process.env.APP_PASSWORD;
 const PORT = process.env.PORT || 3000;
 const app = express();
 const DB = `mongodb+srv://${username}:${password}@cluster0.8sycumm.mongodb.net/?retryWrites=true&w=majority`;
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
 
 // Connections
 mongoose
@@ -21,6 +28,7 @@ mongoose
   .catch((e) => {
     console.log(e);
   });
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`connected at port ${PORT}`);
 });
